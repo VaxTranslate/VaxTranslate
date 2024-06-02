e8dfa886949a700bf173a5868655f83073b95e1e
git-macko <mark1114@uw.edu>
2024-06-02 11:50:40 -0700

    updated login and tailwind.config

--- vaxtranslate/src/components/Login.jsx
+++ vaxtranslate/src/components/Login.jsx
@@ -1,52 +1,87 @@
-import React from 'react';
-import { Link } from "react-router-dom";
-import { FaUserCircle } from "react-icons/fa";
+import React, { useRef, useState } from "react";
+import Upload from "../img/upload.png";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faTimes } from "@fortawesome/free-solid-svg-icons";
+import axios from 'axios'; // Import axios
+import "../App.css";
 
 function LoginPage() {
   return (
-    <div className="bg-blue-300 p-6 rounded-lg shadow-md"> 
-        <div>
-            <input
-              type="text"
-              placeholder="Login ID"
-              className="w-96 p-2 mb-2 border rounded"
-            />
+    <div>
+      <section className="bg-gray-50 dark:bg-gray-900">
+        <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
+          <div className="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
+            <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
+              <h1 className="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
+                Sign in
+              </h1>
+              <form className="space-y-4 md:space-y-6" action="#">
+                <div>
+                  <label htmlFor="email" className="w-4 block mb-1 text-sm font-small text-gray-900 dark:text-white">
+                    Email
+                  </label>
+                  <input
+                    type="email"
+                    name="email"
+                    id="email"
+                    className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                    placeholder="name@company.com"
+                    required
+                  />
+                </div>
+                <div>
+                  <label htmlFor="password" className="w-4 block mb-1 text-sm font-small text-gray-900 dark:text-white">
+                    Password
+                  </label>
+                  <input
+                    type="password"
+                    name="password"
+                    id="password"
+                    placeholder="••••••••"
+                    className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                    required
+                  />
+                </div>
+                <div className="flex items-center justify-between">
+                  <div className="flex items-start">
+                    <div className="flex items-center h-5">
+                      <input
+                        id="remember"
+                        aria-describedby="remember"
+                        type="checkbox"
+                        className="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-primary-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-primary-600 dark:ring-offset-gray-800"
+                        required
+                      />
+                    </div>
+                    <div className="ml-3 text-sm">
+                      <label htmlFor="remember" className="text-gray-500 dark:text-gray-300">
+                        Remember me
+                      </label>
+                    </div>
+                  </div>
+                  <a href="#" className="text-sm font-medium text-primary-600 hover:underline dark:text-primary-500">
+                    Forgot password?
+                  </a>
+                </div>
+                <button
+                  type="submit"
+                  className="w-full text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800"
+                >
+                  Sign in
+                </button>
+                <p className="text-sm font-light text-gray-500 dark:text-gray-400">
+                  Don’t have an account yet?{" "}
+                  <a href="#" className="font-medium text-primary-600 hover:underline dark:text-primary-500">
+                    Sign up
+                  </a>
+                </p>
+              </form>
+            </div>
           </div>
-
-          <div>
-            <input
-              type="password"
-              placeholder="Password"
-              className="w-96 p-2 mb-2 border rounded"
-            />
-          </div>
-
-          <div>
-            <label className="flex items-center mb-2">
-            <input type="checkbox" className="mr-2" />
-                Remember me
-            </label>
-          </div>
-
-            <button className="bg-blue-600 py-2 px-4 rounded">
-                Log in
-            </button>
-          
-
-            <p className="text-sm mt-2">
-              By continuing, you agree to the Terms of use and Privacy Policy.
-            </p>
-            <div className="mt-4">
-              <a href="#" className="text-blue-500">
-                Forgot your password?
-              </a>
-              <span className="mx-2">|</span>
-              <a href="#" className="text-blue-500">
-                Don't have an account? Sign up
-              </a>
         </div>
+      </section>
     </div>
-  );
-}
+  )
+};
 
 export default LoginPage;
