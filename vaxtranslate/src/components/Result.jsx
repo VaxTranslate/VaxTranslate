f10257eaeb7cf1e050efa12bee2403697e894f74
evanc108 <104491330+evanc108@users.noreply.github.com>
2024-07-29 22:26:05 -0700

    homepage setup

--- vaxtranslate/src/components/Result.jsx
+++ vaxtranslate/src/components/Result.jsx
@@ -7,22 +7,17 @@ const Result = () => {
 
   return (
     <div className="d-flex align-items-center justify-content-center mt-5">
-      <div className="w-100" style={{ maxWidth: '900px' }}>
+      <div >
         <div
           className="bg-white shadow-md rounded-md p-5 flex flex-col"
-          style={{
-            width: "900px",
-            borderRadius: "10px",
-            margin: "0 auto",
-            marginTop: "50px",
-          }}
+
         >
           <h2 className="text-center">Translation Result</h2>
           {translatedImage && (
             <img
               src={translatedImage}
               alt="Translated result"
-              style={{ marginTop: "20px", maxWidth: "100%" }}
+              style={{ marginTop: "20px", maxWidth: "200%" }}
             />
           )}
         </div>
