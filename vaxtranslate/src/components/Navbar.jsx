8eee5094cdd5f8c1206109b258a3718d1f96415e
evanc108 <104491330+evanc108@users.noreply.github.com>
2024-07-29 22:35:57 -0700

    navbar

--- vaxtranslate/src/components/Navbar.jsx
+++ vaxtranslate/src/components/Navbar.jsx
@@ -10,73 +10,40 @@ const Navbar = () => {
     };
 
     return (
-        <nav className="bg-vaxBlue dark:bg-gray-900">
-            <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-1">
-                <Link to="/" className="flex items-center space-x-3 rtl:space-x-reverse" style={{ textDecoration: 'none' }}>
-                    <span className="self-center text-2xl font-medium whitespace-nowrap text-white font-poppins text-center leading-custom-lg">
-                        VAX <span className="text-black font-medium leading-custom-lg">Translate</span>
-                    </span>
-                    
-                </Link>
-
-                <button
-                    onClick={handleToggle}
-                    type="button"
-                    className="inline-flex items-center p-2 w-8 h-8 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
-                    aria-controls="navbar-default"
-                    aria-expanded={isOpen}
-                    aria-label="Toggle navigation"
-                >
-                    <span className="sr-only">Open main menu</span>
-                    <svg className="w-4 h-4" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 17 14">
-                        <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M1 1h15M1 7h15M1 13h15" />
-                    </svg>
-                </button>
-                <div className={`w-full md:block md:w-auto ${isOpen ? 'block' : 'hidden'}`} id="navbar-default">
-                    <div className="bg-vaxBlue md:bg-vaxBlue dark:bg-gray-800">
-                        <ul className="font-medium flex flex-col p-1 md:p-0 mt-1 rounded-lg bg-vaxBlue md:flex-row md:space-x-6 rtl:space-x-reverse md:mt-0 md:bg-vaxBlue dark:bg-vaxBlue md:dark:vaxBlue font-poppins">
-                            <li>
-                                <Link
-                                    to="/translate"
-                                    className="block py-1 px-2 text-gray-900 rounded hover:text-blue-700 md:hover:text-white dark:text-white dark:hover:text-blue-500 text-custom-lg font-medium leading-custom-lg"
-                                    style={{ textDecoration: 'none' }}
-                                >
-                                    Translate
-                                </Link>
-                            </li>
-                            <li>
-                                <Link
-                                    to="/dashboard"
-                                    className="block py-1 px-2 text-gray-900 rounded hover:text-blue-700 md:hover:text-white dark:text-white dark:hover:text-blue-500 text-custom-lg font-medium leading-custom-lg"
-                                    style={{ textDecoration: 'none' }}
-                                >
-                                    Dashboard
-                                </Link>
-                            </li>
-                            <li>
-                                <Link
-                                    to="/community"
-                                    className="block py-1 px-2 text-gray-900 rounded hover:text-blue-700 md:hover:text-white dark:text-white dark:hover:text-blue-500 text-custom-lg font-medium leading-custom-lg"
-                                    style={{ textDecoration: 'none' }}
-                                >
-                                    Community
-                                </Link>
-                            </li>
-                            <li>
-                                <Link
-                                    to="/login"
-                                    className="block py-1 px-2 text-gray-900 rounded hover:text-blue-700 md:hover:text-white dark:text-white dark:hover:text-blue-500 text-custom-lg font-medium leading-custom-lg"
-                                    style={{ textDecoration: 'none' }}
-                                >
-                                    <FaUserCircle size={36} />
-                                </Link>
-                            </li>
-                        </ul>
-                    </div>
-                </div>
-            </div>
-        </nav>
-    );
+        <nav className="navbar navbar-expand-lg navbar-light" style={{ backgroundColor: "white", boxShadow: "0 2px 4px rgba(0, 0, 0, 0.2)", fontFamily: 'Inter, sans-serif', fontWeight: "500" }}>
+            <Link to="/" className="navbar-brand ms-30" style={{color: "#3485FF", fontWeight: "bold", fontSize: "30px"}}>
+                VAX Translate
+            </Link>
+            <button
+                className="navbar-toggler"
+                type="button"
+                data-toggle="collapse"
+                data-target="#navbarNav"
+                aria-controls="navbarNav"
+                aria-expanded="false"
+                aria-label="Toggle navigation"
+            >
+                <span className="navbar-toggler-icon"></span>
+            </button>
+            <div className="collapse navbar-collapse justify-content-end align-items-center" id="navbarNav">
+            <ul className="navbar-nav align-items-center">
+                <li className="nav-item mx-2 fw-bold">
+                    <Link className="nav-link" to="/translate">Translate</Link>
+                </li>
+                <li className="nav-item mx-2 fw-bold">
+                    <Link className="nav-link" to="/dashboard">Dashboard</Link>
+                </li>
+                <li className="nav-item mx-2 fw-bold">
+                    <Link className="nav-link" to="/community">Community</Link>
+                </li>
+                <li className="nav-item fw-bold d-flex align-items-center me-3" style={{marginTop: "5px"}}>
+                    <Link className="nav-link" to="/login"><FaUserCircle size={30} /></Link>
+                </li>
+                </ul>
+                
+      </div>
+    </nav>
+  );
 };
 
 export default Navbar;
